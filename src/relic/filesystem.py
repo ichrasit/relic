1fbe70386ef61636428f3764239e4488e0ec0696
Muhammet Raşit Öz <ozrasit@Muhammets-MacBook-Air.local>
2026-08-10 01:34:47 +0300

    feat: validate image files

--- src/relic/filesystem.py
+++ src/relic/filesystem.py
@@ -1,4 +1,20 @@
 from pathlib import Path
+from PIL import Image as PILImage
 
 def file_exists(path: Path) -> bool:
     return path.is_file()
+
+
+
+def is_image(path: Path) -> bool:
+    if not path.is_file():
+        return False
+    
+    try:
+        with PILImage.open(path) as image:
+            image.verify()
+        
+    except (OSError, SyntaxError):
+        return False
+    
+    return True
--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from relic.filesystem import file_exists
-
+from relic.filesystem import file_exists, is_image
+from PIL import Image as PILImage
 def test_file_exists_for_existing_file(tmp_path):
     file_path = tmp_path / "photo.jpg"
     file_path.touch()
@@ -16,3 +16,37 @@ def test_file_exists_for_directoru(tmp_path):
     directory.mkdir()
 
     assert not file_exists(directory)
+
+def test_file_exists_for_valid_image(tmp_path):
+    image_path = tmp_path / "photo.jpg"
+
+    image = PILImage.new("RGB", (100, 100))
+    image.save(image_path)
+    assert file_exists(image_path)
+
+def test_is_image_for_valid_image(tmp_path):
+    image_path = tmp_path / "photo.png"
+
+    image = PILImage.new("RGB", (100, 100))
+    image.save(image_path)
+
+    assert is_image(image_path)
+
+def test_is_image_for_invalid_file(tmp_path):
+    file_path = tmp_path / "not-an-image.txt"
+    file_path.write_text("this is definetly not an image")
+
+    assert not is_image(file_path)
+
+def test_is_image_for_directory(tmp_path):
+    directory = tmp_path / "photos"
+    directory.mkdir()
+
+    assert not is_image(directory)
+
+def test_is_image_for_corrupted_image(tmp_path):
+    image_path = tmp_path / "corrupted.png"
+    image_path.write_bytes(b"\x89PNG\r\n\x1a\nbroken")
+
+    assert not is_image(image_path)
+
