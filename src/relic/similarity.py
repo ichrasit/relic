4e47f97bde31be6446b5af3bb2405732f712cffd
Muhammet Raşit Öz <ozrasit@Muhammets-MacBook-Air.local>
2026-08-10 03:17:25 +0300

    feat: calculate perceptual similarity

---
+++ src/relic/similarity.py
@@ -0,0 +1,7 @@
+def similarity(distance: int, hash_size: int = 64) -> float:
+    if distance < 0:
+        raise ValueError("Distance cannot be negative")
+
+    if distance > hash_size:
+        raise ValueError("Distance cannot exceed hash size")
+    return 1.0 - (distance / hash_size)
