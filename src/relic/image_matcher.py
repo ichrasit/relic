38e3125fc9ccf66a0ea9fdd33a850d04ea00e262
Muhammet Raşit Öz <ozrasit@Muhammets-MacBook-Air.local>
2026-08-10 03:54:03 +0300

    feat: image matcher function added

---
+++ src/relic/image_matcher.py
@@ -0,0 +1,11 @@
+from relic.image import Image
+from relic.similarity import hamming_distance, parse_phash
+from relic.similarity import similarity
+
+def match(first: Image, second: Image) -> float:
+    first_hash = parse_phash(first.phash)
+    second_hash = parse_phash(second.phash)
+
+    distance = hamming_distance(first_hash, second_hash)
+
+    return similarity(distance)
