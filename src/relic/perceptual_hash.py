39e90016854f6a2394903f6491b5fe57a5cce917
Muhammet Raşit Öz <ozrasit@Muhammets-MacBook-Air.local>
2026-08-10 03:07:47 +0300

    feat: add perceptual hash distance

--- src/relic/perceptual_hash.py
+++ src/relic/perceptual_hash.py
@@ -6,3 +6,9 @@ from PIL import Image as PILImage
 def phash(path: Path) -> imagehash.ImageHash:
     with PILImage.open(path) as image:
         return imagehash.phash(image)
+
+def hamming_distance(
+        first: imagehash.ImageHash,
+        second: imagehash.ImageHash,
+) -> int:
+    return first - second
